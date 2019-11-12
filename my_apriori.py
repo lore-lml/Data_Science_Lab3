661a8c776279098a8e418b85a4cb32a26310e846
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2019-11-12 14:18:11 +0100

    script2 done

--- my_apriori.py
+++ my_apriori.py
@@ -1,5 +1,6 @@
 n_transactions = 0
 
+
 def generate_combinations(itemset):
     combinations = []
     for i in range(len(itemset)):
@@ -7,6 +8,7 @@ def generate_combinations(itemset):
             if itemset[i][:-1] == itemset[j][:-1]:
                 combinations.append(itemset[i][:-1] + tuple(sorted([itemset[i][-1], itemset[j][-1]])))
     return combinations
+
 
 def apriori(transactions, minsup):
     global n_transactions
@@ -17,7 +19,7 @@ def apriori(transactions, minsup):
         for item in t:
             single_item_freq[item.strip()] = single_item_freq.get(item.strip(), 0) + 1
 
-    freq_itemset = [{(k,): v for k, v in single_item_freq.items() if v / n_transactions > minsup}]
+    freq_itemset = [{(k,): v for k, v in single_item_freq.items() if v / n_transactions >= minsup}]
 
     while freq_itemset[-1] != {}:
         combinations = generate_combinations(list(freq_itemset[-1].keys()))
@@ -30,7 +32,7 @@ def apriori(transactions, minsup):
                 if c_set.issubset(t_set):
                     tmp_freq[c] = tmp_freq.get(c, 0) + 1
 
-        freq_itemset.append({k: v for k, v in tmp_freq.items() if v / n_transactions > minsup})
+        freq_itemset.append({k: v for k, v in tmp_freq.items() if v / n_transactions >= minsup})
 
     return freq_itemset[:-1]
 
--- script2.py
+++ script2.py
@@ -1,5 +1,9 @@
 import json
-from my_apriori import apriori
+import my_apriori as my_apr
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, fpgrowth
+import numpy as np
+import timeit as tm
 
 
 def get_json(path):
@@ -7,13 +11,64 @@ def get_json(path):
         return json.load(fp)
 
 
-if __name__ == '__main__':
-    dataset = get_json("data_sets/coco.json")
+def get_possible_annotations(transactions):
+    annotations = {item.strip() for t in transactions for item in t}
+    return {a: i for i, a in enumerate(sorted(annotations))}
 
+
+def build_matrix(item_set, transactions):
+    matrix = np.zeros((len(transactions), len(item_set)), dtype=np.int_)
+
+    for i, t in enumerate(transactions):
+        for a in t:
+            annotation_index = item_set[a]
+            matrix[i, annotation_index] = 1
+
+    return matrix
+
+
+def tuples2frozenset(itemset, annotations):
+    fi_myap = set()
+    for it in itemset:
+        fi_myap.update({(v / 5000, frozenset({annotations.get(k_) for k_ in k})) for k, v in it.items()})
+    return fi_myap
+
+if __name__ == '__main__':
     # 2.
-    transactions = list(map(lambda t: t['annotations'], dataset))
+    images = get_json("data_sets/coco.json")
+
+    transactions = [list(set(im['annotations'])) for im in images]
     #print(transactions[:10])
 
-    freq = apriori(transactions, 0.02)
-    print(sum(list(map(lambda x: len(x), freq))))
+    # 3.
+    freq = my_apr.apriori(transactions, 0.02)
+    # print(sum(list(map(lambda x: len(x), freq))))
     print(freq)
+
+    """itemset = {'baseball bat', 'baseball glove', 'bench', 'person'}
+    with_itemset = [im['image_id'] for im in images if itemset.issubset(set(im['annotations']))]
+    print(with_itemset[30])"""
+
+    # 4.
+    possible_annotations = get_possible_annotations(transactions)
+    #print(len(possible_annotations))
+    matrix = build_matrix(possible_annotations, transactions)
+    # print(matrix[:10, :10])
+    df = pd.DataFrame(data=matrix, columns=possible_annotations.keys())
+    ap = apriori(df, min_support=0.02)
+    fpg = fpgrowth(df, min_support=0.02)
+
+    my_tuple = tuples2frozenset(freq, possible_annotations)
+    ap_tuple = {tuple(t) for t in ap.values}
+    fpg_tuple = {tuple(t) for t in fpg.values}
+
+    # Tutti e 3 gli algoritmi hanno dato risultato equivalente
+    print(f"fpg_tuple == ap_tuple: {fpg_tuple == ap_tuple}")
+    print(f"my_tuple == ap_tuple: {my_tuple == ap_tuple}")
+
+    print("fpgrowth", tm.timeit(lambda: fpgrowth(df, 0.02), number=1))
+    print("apriori", tm.timeit(lambda: apriori(df, 0.02), number=1))
+    print("my_apriori", tm.timeit(lambda: my_apr.apriori(transactions, 0.02), number=1))
+
+
+
